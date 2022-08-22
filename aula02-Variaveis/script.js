cef0f7e1300730ec45d7abbc8a561ade8c161e38
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-22 17:34:20 -0300

    Aual 02

---
+++ aula02-Variaveis/script.js
@@ -0,0 +1,13 @@
+// Tipos primitivos
+
+// boolean
+var vOuf = false;
+console.log(typeof(vOuf));
+
+//number
+var num = 1;
+console.log(typeof(num));
+
+//string
+var nome = 'daniel';
+console.log(typeof(daniel))
