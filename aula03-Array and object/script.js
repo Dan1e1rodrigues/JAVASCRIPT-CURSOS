adf7de1e9d19e55b848c8eb74a5432b9f15037f0
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-24 17:14:50 -0300

    Update script.js

--- aula03-Array and object/script.js
+++ aula03-Array and object/script.js
@@ -2,6 +2,26 @@
 /*let array=['string', 1, true];
 console.log(array);*/
 
-//Array pode guardar varios tipos de dados
+//Array pode guardar varios tipos de dados.
+
 var array = ['string', 1, true, [array1], [array2], [array3], [array4]];
 console.log(array);
+
+var xicara = {
+    cor:'azul',
+    tamanho:'p',
+    funcao: tomarcafe()
+}
+
+//A função de um array
+var xicara = {cor:'azul', tamanho:'p', funcao: tomarcafe()}
+
+//Dados de um array
+var cor = xicara.cor;
+var tamanho = xicara.tamanho;
+var funcao = tomatCafe();
+
+//Manipulando objetos utilizando chaves
+var xicara = {cor: 'azul', tamanho: 'p', funcao: tomarcafe()}
+
+var {cor, tamanho, funcao} = xicara;
