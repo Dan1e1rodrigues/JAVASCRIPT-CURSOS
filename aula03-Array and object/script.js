4e33208fc59a310ea4cf7991270998403576d825
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-24 15:40:29 -0300

    Exercicio 03 array

---
+++ aula03-Array and object/script.js
@@ -0,0 +1,7 @@
+//O que são vetores ou array
+/*let array=['string', 1, true];
+console.log(array);*/
+
+//Array pode guardar varios tipos de dados
+var array = ['string', 1, true, [array1], [array2], [array3], [array4]];
+console.log(array);
