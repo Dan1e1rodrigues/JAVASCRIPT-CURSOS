c518707a78a4a25b9013603d0eeb83b94f4b4016
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-25 17:28:36 -0300

    Js first commit

---
+++ aula04-Estruturas condicionais/script.js
@@ -0,0 +1,46 @@
+//Minhas duas variaveis 
+/*
+var jogador01 = prompt ("Qual foi a pontuação do jogador 01 ?");
+
+var jogador02 = prompt ("Qual foi a pontuação do jogador 02 ?");
+
+//placar
+var placar;
+
+//Minhas condições 
+if (jogador01 > jogador02) {
+    document.write("O jogador 01 VENCEU!!!");
+}
+else{
+    document.write("O jogador 02 VENCEU!!!");
+}
+
+//document.write("Pontos do jogador 01 " + jogador01 + " pontos do jogador 02" + jogador02 + "");
+
+//Switch Nova função
+/*switch (placar) {
+case placar = jogador01 > jogador02;
+    document.write('Jogador 01 ganhou');
+    break;
+case placar = jogador02 > jogador01
+    document.write('Jogador 02 ganhou');
+    break;
+default:
+    document.write('Ninguém ganhou')
+}
+
+
+// for/in executa repetição a partir de uma propriedade
+
+//com array
+for (let i in array) {
+    //posso usar também document.write
+    console.log(i)
+}
+
+//com object
+for (i of object.propriedade1 ) {
+    console.log(i);
+}
+*/
+
