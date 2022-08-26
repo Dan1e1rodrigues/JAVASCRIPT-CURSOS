376938516b0533e1f300207e1b002b4632774c52
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-26 16:22:03 -0300

    Aula de funções

---
+++ aula05-Funçoes/script.js
@@ -0,0 +1,13 @@
+//Uma das formas de fazer
+/*function funcao() {
+    console.log('Óla, tudo bem? ');
+}
+
+funcao();
+*/
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Tudo certo', 'Daniel!')
