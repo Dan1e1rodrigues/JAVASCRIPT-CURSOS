7aeee44262391f5bd583cbe6072740d29807cf38
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-26 16:55:46 -0300

    Criando uma calculadora

---
+++ aula06-Aprofundando em funçoes/script.js
@@ -0,0 +1,25 @@
+// tipos de funçao
+/*
+function funcao() {
+    console.log('Mensagem de função declarativa')
+}
+
+funcao()
+*/
+
+//expressoes de fução
+//com nomeação
+/*
+var funcao = function() {
+    console.log('Sou uma mensagem de função de exspressão!')
+}
+
+funcao();
+*/
+
+//arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+
+funcao();
