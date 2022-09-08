fac42cebc89bde810680b51d24e3993491815672
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-09-08 17:41:11 -0300

    Primeiras interações com JS
    Fazendo os exercícios do bootcamp, aula Programação para internet com JavaScript array

---
+++ aula_javascript/script.js
@@ -0,0 +1,38 @@
+
+let nome = prompt('Qual seu nome ?');
+let idade = prompt('Qual a sua idade ?');
+
+alert('Olá, ' + nome + " PRAZER, alias você tem " + idade + " Acertei ??");
+
+
+//Comentarios 1!
+/*Comentario 2!*/
+
+//Exercicio da aula 01
+/*
+var nome = 'Daniel Rodrigues';
+
+var n1 = 5;
+var n2 = 45;
+
+var frase = 'Brasil e o pais mais incrivel do mundo todo';
+
+alert (nome + ' Tem ' + idade + ' Anos');
+
+alert (idade + idade2);
+
+console.log(nome);
+
+console.log(n1 * n2);
+
+console.log(frase.toLocaleLowerCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
+
+//Exercicio 02 Array e discionario
+//Push para colocar 
+//length para ver o tamanho da lista
+//Reverse para tirar
+var lista = ["Banana", "Abacaxi", "Maça", "Laranja"];
+
+console.log(lista);
