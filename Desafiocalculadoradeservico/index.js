d4f55ec081ed5009efed735c1d6074bf5e543fa3
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-09-09 17:27:08 -0300

    commit desafio
    Nessa fase estou declarando algumas variáveis, e organizando variáveis como valores , hora e serviços

---
+++ Desafiocalculadoradeservico/index.js
@@ -0,0 +1,22 @@
+/*
+Criar uma calculadora de valor a ser cobrado do cliente.
+O valor do serviço pode variar conforme o prazo.
+Os valores e prazos devem ser armazenados em uma matriz.
+O cálculo deve ficar obrigatoriamente em uma função.
+*/
+
+//Solicitação para o usuario 
+
+//Solicitação ao usuario referente a serviço.
+let servico = parseInt(prompt('Escolha um dos nossos serviços [1], [2], [3] '));
+
+//Solicitação de horas sugeridas para usuario.
+let horas = parseInt(prompt('Quantidade de horas de espera?[12][24][48][72]horas'));
+
+let calculoservico = calculo(servico, horas);
+alert(calculoservico);
+
+//Valores dos serviços.
+//declarando os valores 
+let valores = ['R$10', 'R$20', 'R$30', 'R$40', 'R$50', 'R$60', 'R$70', 'R$80', 'R$90', 'R$100'];
+
