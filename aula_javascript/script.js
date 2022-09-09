5d84a80afae1b01124820399cbe40c5eb6b54b9d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-09-09 17:02:06 -0300

    Aula de array

--- aula_javascript/script.js
+++ aula_javascript/script.js
@@ -1,8 +1,8 @@
 
-let nome = prompt('Qual seu nome ?');
-let idade = prompt('Qual a sua idade ?');
+// let nome = prompt('Qual seu nome ?');
+// let idade = prompt('Qual a sua idade ?');
 
-alert('Olá, ' + nome + " PRAZER, alias você tem " + idade + " Acertei ??");
+// alert('Olá, ' + nome + " PRAZER, alias você tem " + idade + " Acertei ??");
 
 
 //Comentarios 1!
@@ -33,6 +33,23 @@ alert(frase.replace("Japão", "Brasil"));
 //Push para colocar 
 //length para ver o tamanho da lista
 //Reverse para tirar
-var lista = ["Banana", "Abacaxi", "Maça", "Laranja"];
 
-console.log(lista);
+//Armazeno a informação com o array
+// var lista = ["banana", "abacaxi", "maça", "laranja"];
+// //Com o push adiciono novas informações 
+// lista.push("Tomate", "rucula", "laranja");
+
+// console.log(lista);
+// console.log(lista.toString());
+// //Quero que fique saparado por um sinal (_ - - + * & ¨ % /) entre outros
+// console.log(lista.join(" _ "));
+
+//Biblioteca
+// var fruta = {nome:"Abacate", cor:"Verde"};
+// console.log(fruta.nome);
+// alert(fruta.cor); 
+
+//Segundo exemplo de biblioteca
+var frutas = [{nome:"Abacate", cor:"Verde",}, {nome:"Uva", cor:"Roxa"}, {nome:"Maça", cor:"Vermelha",}, {nome:"Laranja", cor:"Laranja",}, {nome:"Tangerina", cor:"Laranja",}];
+console.log(frutas);
+alert(frutas[4].nome);
---
+++ condicionais, laços de repetição/script.js
@@ -0,0 +1,23 @@
+// var idade = prompt("Qual e a sua idade?");
+// if(idade >= 18) {
+//     alert("Maior de idade, já pode se alistar ");
+// }else {
+//     alert("Menor de idade, ainda não tem idade suficiente para se alistar ");
+// }
+
+// document.write("Sua idade é " + idade + "");
+
+//Count em outras palavras contador
+// var count = 0;
+// while (count <= 10) {
+//     alert(count);
+//     count++;
+// };
+
+//como faço para saber a data?
+var data = new Date();
+alert(data);
+//Para saber o dia da Semana
+alert(data.getMonth()+1);
+//Minutos
+alert(data.getMinutes());
