6f184e30fb9e202dc3e5146acca19e9802aebe44
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-31 16:15:26 -0300

    commit
    Voltando a treinar logica com JS e preparando para prova de Java.

---
+++ interagindo btn/script.js
@@ -0,0 +1,8 @@
+// Declaramos qual tag chamariamos, button.
+const btn = document.querySelector('button');
+
+// Declaramos o evento que ocorreria com a interação do botão.
+btn.addEventListener('click', function(){
+    // Para que assim a mensagem fosse exibida.
+    alert("Você apertou o botão");
+});
